5dc4483c558b238216bc001be004fed5a9765325
Tyoukai <zhang_k_w@163.com>
2021-10-24 15:07:01 +0800

    feat:平迁

---
+++ src/main/java/com/fast/gateway/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.fast.gateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/rate")
+    public Mono<String> rateLimit() {
+        return Mono.just("rate-limit");
+    }
+
+    @RequestMapping("/logRecord")
+    public Mono<String> logRecord() {
+        return Mono.just("log-record");
+    }
+}
--- src/test/java/flashgateway/FlashGatewayApplicationTests.java
+++ src/test/java/flashgateway/FlashGatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package flashgateway;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FlashGatewayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package flashgateway;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FlashGatewayApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
