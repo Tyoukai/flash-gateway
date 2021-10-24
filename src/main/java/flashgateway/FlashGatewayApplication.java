01823b3eb669490a8e72a0148eeddbd7ae35450a
Tyoukai <zhang_k_w@163.com>
2021-10-24 13:28:14 +0800

    feat:flash-gateway init

---
+++ src/main/java/flashgateway/FlashGatewayApplication.java
@@ -0,0 +1,13 @@
+package flashgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flashgateway/FlashGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package flashgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
