747b2ed6b1e59105246830068cb0616f1756e9e0
Tyoukai <zhang_k_w@163.com>
2022-01-18 09:36:14 +0800

    鉴权相关逻辑

--- src/main/java/com/fast/gateway/filter/AuthGatewayFilterFactory.java
+++ src/main/java/com/fast/gateway/filter/AuthGatewayFilterFactory.java
@@ -1,7 +1,33 @@
 package com.fast.gateway.filter;
 
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
 /**
  * 鉴权相关的filter
  */
-public class AuthGatewayFilterFactory {
+@Component
+public class AuthGatewayFilterFactory extends AbstractGatewayFilterFactory<AuthGatewayFilterFactory.Config> {
+
+    public AuthGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return null;
+    }
+
+    public static class Config {
+        private String appKey;
+
+        public String getAppKey() {
+            return appKey;
+        }
+
+        public void setAppKey(String appKey) {
+            this.appKey = appKey;
+        }
+    }
 }
