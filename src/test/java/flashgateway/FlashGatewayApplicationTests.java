a581730880b706fed284221aafd32e96f495a769
Tyoukai <zhang_k_w@163.com>
2022-01-20 09:26:02 +0800

    增加单元测试

--- src/test/java/flashgateway/FlashGatewayApplicationTests.java
+++ src/test/java/flashgateway/FlashGatewayApplicationTests.java
@@ -1,13 +1,26 @@
-//package flashgateway;
-//
-//import org.junit.jupiter.api.LogTest;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class FlashGatewayApplicationTests {
-//
-//	@LogTest
-//	void contextLoads() {
-//	}
-//
-//}
+package flashgateway;
+
+import com.fast.gateway.Application;
+import com.fast.gateway.entity.ApiQuotaLimitDO;
+import com.fast.gateway.service.ApiQuotaLimitService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Map;
+
+@SpringBootTest(classes = Application.class)
+@RunWith(SpringRunner.class)
+public class FlashGatewayApplicationTests {
+
+    @Autowired
+    private ApiQuotaLimitService apiQuotaLimitService;
+
+    @Test
+    public void listAllQuotaLimitConfigTest() {
+        Map<String, ApiQuotaLimitDO> map = apiQuotaLimitService.listAllQuotaLimitConfig();
+    }
+
+}
