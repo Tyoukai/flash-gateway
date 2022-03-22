9cb9340bf78f4371c712fe82c2f9ae4cb86538c0
Tyoukai <zhang_k_w@163.com>
2022-03-22 09:35:00 +0800

    限额页面完善

--- frontend/src/components/QuotaLimit.vue
+++ frontend/src/components/QuotaLimit.vue
@@ -12,17 +12,17 @@
       </tr>
       </thead>
       <tbody>
-      <tr>
-        <td>1</td>
-        <td>quota.limit.test.api</td>
-        <td>userId:6666</td>
-        <td>50</td>
-        <td>1800</td>
-        <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modifyQuotaLimit">修改</button></td>
+      <tr v-for="quotaLimit in quotaLimitList" v-bind:key="quotaLimit">
+        <td>{{quotaLimit.id}}</td>
+        <td>{{quotaLimit.api}}</td>
+        <td>{{quotaLimit.quotaKey}}</td>
+        <td>{{quotaLimit.quota}}</td>
+        <td>{{quotaLimit.timeSpan}}</td>
+        <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modifyQuotaLimit">修改</button><button type="button" class="btn btn-danger">删除</button></td>
       </tr>
       </tbody>
     </table>
-    <div><button type="button" style="float:right" class="btn btn-success" data-toggle="modal" data-target="#modifyQuotaLimit">新增</button></div>
+    <div><button type="button" style="float:right" class="btn btn-success" data-toggle="modal" data-target="#addQuotaLimit">新增</button></div>
     <div class="modal fade" id="modifyQuotaLimit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
@@ -36,44 +36,148 @@
           </div>
           <div class="modal-body">
             <form class="bs-example bs-example-form" role="form">
+              <div class="input-group" style="display:none;">
+                <span class="input-group-addon">id</span>
+                <input id="modifyId" type="text" class="form-control" placeholder="">
+              </div>
               <div class="input-group">
                 <span class="input-group-addon">api</span>
-                <input type="text" class="form-control" placeholder="">
+                <input id="modifyApi" disabled="disabled" type="text" class="form-control" placeholder="">
               </div>
               <br>
               <div class="input-group">
                 <span class="input-group-addon">限额key</span>
-                <input type="text" class="form-control" placeholder="">
+                <input id="modifyKey" type="text" class="form-control" placeholder="">
               </div>
               <br>
               <div class="input-group">
                 <span class="input-group-addon">额度</span>
-                <input type="text" class="form-control" placeholder="">
+                <input id="modifyQuota" type="text" class="form-control" placeholder="">
               </div>
               <br>
               <div class="input-group">
                 <span class="input-group-addon">时间（秒）</span>
-                <input type="text" class="form-control" placeholder="">
+                <input id="modifyTimeSpan" type="text" class="form-control" placeholder="">
               </div>
             </form>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-default" data-dismiss="modal">关闭
             </button>
-            <button type="button" class="btn btn-primary">
+            <button type="button" class="btn btn-success" data-dismiss="modal">
               确认
             </button>
           </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal -->
+        </div>
+      </div>
+    </div>
+    <div class="modal fade" id="addQuotaLimit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+              &times;
+            </button>
+            <h4 class="modal-title" id="myModalLabel">
+              限流配置
+            </h4>
+          </div>
+          <div class="modal-body">
+            <form class="bs-example bs-example-form" role="form">
+              <div class="input-group" style="display:none;">
+                <span class="input-group-addon">id</span>
+                <input id="addId" type="text" class="form-control" placeholder="">
+              </div>
+              <div class="input-group">
+                <span class="input-group-addon">api</span>
+                <input id="addApi" type="text" class="form-control" placeholder="">
+              </div>
+              <br>
+              <div class="input-group">
+                <span class="input-group-addon">限额key</span>
+                <input id="addKey" type="text" class="form-control" placeholder="">
+              </div>
+              <br>
+              <div class="input-group">
+                <span class="input-group-addon">限额值</span>
+                <input id="addQuota" type="text" class="form-control" placeholder="">
+              </div>
+              <br>
+              <div class="input-group">
+                <span class="input-group-addon">时间（秒）</span>
+                <input id="addTimeSpan" type="text" class="form-control" placeholder="">
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">关闭
+            </button>
+            <button type="button" class="btn btn-success" data-dismiss="modal">
+              确认
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'QuotaLimit'
+  name: 'QuotaLimit',
+  data () {
+    return {
+      quotaLimitList: [
+        {
+          id: '',
+          api: '',
+          quotaKey: '',
+          quota: '',
+          timeSpan: ''
+        }
+      ]
+    }
+  },
+  mounted: function () {
+    this.listQuotaLimitConfig()
+  },
+  methods: {
+    listQuotaLimitConfig: function () {
+      const axios = require('axios').default
+      axios
+        .get('http://localhost:8100/gateway/list-quota-limit-config')
+        .then(response => {
+          console.log(response)
+          this.quotaLimitList = response.data
+        })
+        .cached(function (error) {
+          console.log(error)
+        })
+    }
+  }
 }
+$(function () {
+  $('#modifyQuotaLimit').on('show.bs.modal', function (event) {
+    var rateLimitButton = $(event.relatedTarget); //触发事件的按钮
+    var id = rateLimitButton.closest('tr').find('td').eq(0).text();
+    var api = rateLimitButton.closest('tr').find('td').eq(1).text();
+    var quotaKey = rateLimitButton.closest('tr').find('td').eq(2).text();
+    var quota = rateLimitButton.closest('tr').find('td').eq(3).text();
+    var timeSpan = rateLimitButton.closest('tr').find('td').eq(4).text();
+    $("#modifyId").attr("value", id)
+    $("#modifyApi").attr("value", api)
+    $("#modifyKey").attr("value", quotaKey)
+    $("#modifyQuota").attr("value", quota)
+    $("#modifyTimeSpan").attr("value", timeSpan)
+  }),
+  $('#addQuotaLimit').on('show.bs.modal', function (event) {
+    $("#addId").val("")
+    $("#addApi").val("")
+    $("#addKey").val("")
+    $("#addQuota").val("")
+    $("#addTimeSpan").val("")
+  })
+})
 </script>
 
 <style scoped>
--- src/main/java/com/fast/gateway/controller/ConfigController.java
+++ src/main/java/com/fast/gateway/controller/ConfigController.java
@@ -1,5 +1,6 @@
 package com.fast.gateway.controller;
 
+import com.fast.gateway.entity.ApiQuotaLimitDO;
 import com.fast.gateway.entity.ApiRateLimitDO;
 import com.fast.gateway.service.ApiRateLimitService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,7 +20,7 @@ public class ConfigController {
     private ApiRateLimitService apiRateLimitService;
 
     @RequestMapping("/list-rate-limit")
-    public Mono<List<ApiRateLimitDO>> mapRateLimitConfig() {
+    public Mono<List<ApiRateLimitDO>> listRateLimitConfig() {
         return Mono.just(apiRateLimitService.listAllApiRateLimitConfig());
     }
 
@@ -35,5 +37,28 @@ public class ConfigController {
     @RequestMapping("/delete-rate-limit")
     public Mono<Boolean> deleteRateLimitConfig(@RequestParam Integer id, @RequestParam String api) {
         return Mono.just((apiRateLimitService.deleteRateLimitConfig(id, api)));
+    }
+
+    @RequestMapping("/list-quota-limit")
+    public Mono<List<ApiQuotaLimitDO>> listQuotaLimitConfig() {
+        List<ApiQuotaLimitDO> list = new ArrayList<>();
+        ApiQuotaLimitDO apiQuotaLimitDO = new ApiQuotaLimitDO();
+        apiQuotaLimitDO.setId(1);
+        apiQuotaLimitDO.setApi("web.test.api");
+        apiQuotaLimitDO.setQuotaKey("key:value");
+        apiQuotaLimitDO.setQuota(10);
+        apiQuotaLimitDO.setTimeSpan(1800);
+
+        ApiQuotaLimitDO apiQuotaLimitDO2 = new ApiQuotaLimitDO();
+        apiQuotaLimitDO2.setId(2);
+        apiQuotaLimitDO2.setApi("web.test.api2");
+        apiQuotaLimitDO2.setQuotaKey("key:value2");
+        apiQuotaLimitDO2.setQuota(10);
+        apiQuotaLimitDO2.setTimeSpan(1800);
+
+        list.add(apiQuotaLimitDO);
+        list.add(apiQuotaLimitDO2);
+
+        return Mono.just(list);
     }
 }
